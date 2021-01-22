37c7bfee399b09550662fc6a4a7f54af45dedcdd
cmatchneer <cmatchneer@yahoo.com>
2021-01-22 16:05:30 -0500

    style issues

--- inventory/products/models.py
+++ inventory/products/models.py
@@ -3,11 +3,10 @@ from django.db import models
 # Create your models here.
 class Product (models.Model):
     title = models.CharField(max_length=120)
-    description = models.TextField(blank =True, null =True, default='test')
+    description = models.CharField(max_length=255,blank =True, null =True, default='test')
     price = models.DecimalField(decimal_places=2,max_digits=10)
     quantity= models.IntegerField(default=0) 
     available = models.BooleanField(default=True)
     date = models.DateField(auto_now=False,auto_now_add=False, default="2021-11-1")
 
-    # def get_absolute_url(self):
-    #     return reverse("product:product-detail", kwargs={"id": self.id})
+    

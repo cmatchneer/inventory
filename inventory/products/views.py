d4ff7b453b48b28c55027ba2e9e11b1010e1d311
cmatchneer <cmatchneer@yahoo.com>
2021-01-22 02:49:29 -0500

    basics work needs styleing

--- inventory/products/views.py
+++ inventory/products/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+
 from .models import Product
 from .form import ProductForm
-# Create your views here.
 
+
+#confim user logged.
+@login_required()
+#add new product form
 def product_form_view(request):
+    
     form = ProductForm(request.POST or None)
+
     if form.is_valid():
         form.save()
         form = ProductForm()
@@ -12,13 +20,18 @@ def product_form_view(request):
         'form': form
     }
     return render(request, "product/product_form.html", context)
+
+@login_required()
+#full product list
 def product_list_view(request):
     queryset=Product.objects.all()
     context={
-      "product_list":queryset
+      "product_list":queryset,
 
     }
     return render(request,"product/product_detail.html",context)
+@login_required()
+#delete product
 def product_delete_view(request, id):
     obj = get_object_or_404(Product, id=id)
     if request.method == "POST":
@@ -29,7 +42,8 @@ def product_delete_view(request, id):
     }
     return render(request, "product/product_delete.html", context)
 
- 
+@login_required()
+#update product 
 def product_update_view(request, id=id):
     obj = get_object_or_404(Product, id=id)
     form = ProductForm(request.POST or None, instance=obj)

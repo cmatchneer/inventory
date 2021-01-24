1a96bfe898d90af2a16fa2f1f9b4515ffc44036a
cmatchneer <cmatchneer@yahoo.com>
2021-01-24 02:27:03 -0500

    test

--- inventory/products/models.py
+++ inventory/products/models.py
@@ -5,7 +5,6 @@ import datetime
 # Create your models here.
 class Product (models.Model):
     title = models.CharField(max_length=120)
-    description = models.CharField(max_length=255,blank =True, null =True, default='test')
     price = models.DecimalField(decimal_places=2,max_digits=10)
     quantity= models.IntegerField(default=0) 
     available = models.BooleanField(default=True)
